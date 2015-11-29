9a1a391a2e9fa9fad37a704263b2195f90f9dc61
Mayank Patel <maknahar@yahoo.in>
2015-11-29 06:46:32 +0530

    changed commit shortcuts

--- commit.py
+++ commit.py
@@ -50,18 +50,18 @@ while True:
             mfile= line.split("deleted:   ",1)[1].strip("\n")   
             
                  
-        commit_msg = raw_input("\nCommit Title\n a: Added new feature\n b: Bug Fix\n c: Build Issue\n d: Debuging\n i: ignore this file\n p: Performance Improvement\n q: same as last file\n r: Reorganized the code\n t: Added test case\nEnter Commit Title: ")
+        commit_msg = raw_input("\nCommit Shortcuts\n a: Added new File <file name>\n b: Bug Fix\n c: Build Issue\n d: Deleted file <file name>\n i: ignore this file from commit\n p: Performance Improvement\n q: same as last file\n r: Renamed file <file name>\n t: Added test case\nEnter Commit Title: ")
         
         if commit_msg.strip()!="i" and commit_msg.strip()!='':
             push = True
             if commit_msg.strip()=="a":
-                commit_msg="Added new feature"
+                commit_msg="Added new File: "+ mfile
             if commit_msg.strip()=="b":
                 commit_msg="Bug Fix"
             if commit_msg.strip()=="c":
                 commit_msg="Build Issue"
             if commit_msg.strip()=="d":
-                commit_msg="Debuging"
+                commit_msg="Deleted File: "+ mfile
             if commit_msg.strip()=="p":
                 commit_msg="Performance Improvement"
             if commit_msg.strip()=="q":
@@ -69,7 +69,7 @@ while True:
                 description=last_commit_desc
                 new_msg=False
             if commit_msg.strip()=="r":
-                commit_msg="Refactoring the code"
+                commit_msg="Renamed file: "+mfile
             if commit_msg.strip()=="t":
                 commit_msg="Added test case"
 
@@ -91,6 +91,7 @@ if push==True:
     print bcolors.OKGREEN+"\n☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺"+bcolors.ENDC
     print bcolors.OKBLUE+ "Finished Committing all new and modified Files."+bcolors.ENDC
     print bcolors.OKGREEN+"☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ ☺ \n"+bcolors.ENDC
+
 print bcolors.BOLD+"Git Status Now:\n"+bcolors.ENDC
 os.system("git status")
 
