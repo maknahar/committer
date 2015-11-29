8aaaf0f2543ac7f022f6770ab3a222fd3eacd394
Mayank Patel <maknahar@yahoo.in>
2015-11-29 08:24:03 +0530

    added quit option in each commit

--- commit.py
+++ commit.py
@@ -65,7 +65,7 @@ while True:
             mfile= line.split("deleted:   ",1)[1].strip("\n")   
             
                  
-        commit_msg = raw_input("\nCommit Shortcuts\n a: Added new File <file name>\n d: Deleted file <file name>\n i: ignore this file from commit\n p: Performance Optimization\n q: same as last file\n r: Renamed file <file name>\nEnter Commit Title: ")
+        commit_msg = raw_input("\nCommit Shortcuts\n a: Added new File <file name>\n d: Deleted file <file name>\n i: ignore this file from commit\n p: Performance Optimization\n q: Quit Commiting\n r: Renamed file <file name>\n s: Same as last file\nEnter Commit Title: ")
         
         if commit_msg.strip()!="i" and commit_msg.strip()!='':
             push = True
@@ -75,14 +75,14 @@ while True:
                 commit_msg="Deleted File: "+ mfile
             if commit_msg.strip()=="p":
                 commit_msg="Performance Optimization"
-            if commit_msg.strip()=="q":
+            if commit_msg.strip()=="s":
                 commit_msg=last_commit_title
                 description=last_commit_desc
                 new_msg=False
             if commit_msg.strip()=="r":
                 commit_msg="Renamed file: "+mfile
-            if commit_msg.strip()=="t":
-                commit_msg="Added test case"
+            if commit_msg.strip()=="q":
+                quit()
 
             if new_msg:
                 description = raw_input("\nEnter Commit Description(Optional):")
